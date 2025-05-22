8acadd8803feae82a8247b11b02198b542678849
oxCucu <0xcucurbitaceae@gmail.com>
2025-05-22 16:34:50 +0200

    next config

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+// no idea why this is .mjs, but not changing it until i need to
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {},
+    serverComponentsExternalPackages: [`require-in-the-middle`],
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+    return config;
+  },
+  productionBrowserSourceMaps: true,
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+    buildActivity: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+
+export default nextConfig
